1a6bc1b4783094781395aa58fe8744bc7366ab3e
programeri <programeri@ucione.local>
2017-05-03 19:37:56 +0200

    Azurirane route u web.php

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,32 @@
 */
 
 
-Route::get('/', function () {
-    $items = DB::table('items')->get();
-	
-	return view('welcome', compact('items'));
+Route::get('/', function ()
+{
+	return view('welcome');
 });
 
-Route::get('item/{id}', function($id) {
-	$item = DB::table('items')->find($id);
-	
-	return view('items.show', compact('item'));
-});
+// popis svih Items
+
+Route::get ('/items', 'ItemsController@index');
+
+// Dodavanje novog Items
+
+Route::get ('/items/new', 'ItemsController@create');
+Route::post ('/items/new', 'ItemsController@store');
+
+// prikaz određenog Items
+
+Route::get ('/items/{id}', 'ItemsController@show');
+
+// Uređivanje (EDIT) postojećeg Items
+
+Route::get ('/item/{id}/edit', 'ItemsController@edit');
+Route::post ('/item/{id}/edit', 'ItemsController@update');
+
+//Brisanje Items
+
+Route::delete ('/item/{id}', 'ItemsController@destroy');
 
 
-
-
-
-
-
-
-
+?>
